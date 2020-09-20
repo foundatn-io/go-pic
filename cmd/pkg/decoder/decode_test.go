8458fd1cad4ac7b183520e6370b8568cc48c85c6
mitchep1 <patrick.mitchell@anz.com>
2020-09-20 21:31:23 +1000

    Add basic record resolution tests

--- cmd/pkg/decoder/decode_test.go
+++ cmd/pkg/decoder/decode_test.go
@@ -3,6 +3,7 @@ package decoder
 import (
 	"bytes"
 	"log"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -52,6 +53,68 @@ func TestDecoder_Unmarshal(t *testing.T) {
 			// 	DUMMY7 string `pic:"10"`
 			// 	DUMMY8 string `pic:"1"`
 			// }
+		})
+	}
+}
+
+func Test_decoder_findDataRecord(t *testing.T) {
+	tests := []struct {
+		name string
+		line string
+		c    *copybook.Copybook
+		want *copybook.Record
+	}{
+		{
+			name: "BasicPICStringSingle",
+			line: "000600         10  DUMMY-1                PIC X.                  00000167",
+			c:    copybook.New("dummy", template.CopyBook),
+			want: &copybook.Record{
+				Num:     600,
+				Level:   10,
+				Name:    "DUMMY-1",
+				Picture: reflect.String,
+				Length:  1,
+			},
+		}, {
+			name: "BasicPICStringParentheses",
+			line: "000610         10  DUMMY-2                PIC X(3).               00000168",
+			c:    copybook.New("dummy", template.CopyBook),
+			want: &copybook.Record{
+				Num:     610,
+				Level:   10,
+				Name:    "DUMMY-2",
+				Picture: reflect.String,
+				Length:  3,
+			},
+		}, {
+			name: "BasicPICIntParentheses",
+			line: "000620         10  DUMMY-3                PIC 9(7).               00000169",
+			c:    copybook.New("dummy", template.CopyBook),
+			want: &copybook.Record{
+				Num:     620,
+				Level:   10,
+				Name:    "DUMMY-3",
+				Picture: reflect.Int,
+				Length:  7,
+			},
+		}, {
+			name: "BasicPICStringMulti",
+			line: "000640         10  DUMMY-5                PIC XX.                 00000171",
+			c:    copybook.New("dummy", template.CopyBook),
+			want: &copybook.Record{
+				Num:     640,
+				Level:   10,
+				Name:    "DUMMY-5",
+				Picture: reflect.String,
+				Length:  2,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := (&decoder{}).findDataRecord(tt.line, tt.c)
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
