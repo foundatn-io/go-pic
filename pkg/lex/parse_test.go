e63405855bb9da8d0d5eb8a7cf5994d0e9248165
mitchep1 <patrick.mitchell@anz.com>
2021-01-17 20:34:12 +1100

    Add occurs tests (though no assertions yet)

--- pkg/lex/parse_test.go
+++ pkg/lex/parse_test.go
@@ -72,10 +72,11 @@ func Test_Parse(t *testing.T) {
 			in: NewTree(
 				New("test",
 					`000160     05  DUMMY-GROUP-1.                                           00000115
-000170         10  DUMMY-SUB-GROUP-1.                                   00000116
+000170         10  DUMMY-SUB-GROUP-1.                                00000116
 000180             15  DUMMY-GROUP-1-OBJECT-A   PIC 9.               00000117
-000190             15  DUMMY-GROUP-1-OBJECT-B   PIC X.                  00000118
-000200             15  DUMMY-GROUP-1-OBJECT-C   PIC 9.               00000119`)),
+000190             15  DUMMY-GROUP-1-OBJECT-B   PIC X.               00000118
+000200             15  DUMMY-GROUP-1-OBJECT-C   PIC 9.               00000119
+		`)),
 		}, {
 			name: "RedefinesWithParentheses",
 			in: NewTree(
@@ -84,7 +85,8 @@ func Test_Parse(t *testing.T) {
 001070         10  DUMMY-GROUP-2-OBJECT-D       PIC X.                  00000219
 001130         10  DUMMY-GROUP-2-OBJECT-E       PIC X(4).               00000225
 001140         10  DUMMY-GROUP-2-OBJECT-F       REDEFINES               00000226
-001150              DUMMY-GROUP-2-OBJECT-E      PIC X(4).               00000227`)),
+001150             DUMMY-GROUP-2-OBJECT-E       PIC X(4).               00000227
+		`)),
 		}, {
 			name: "Redefines",
 			in: NewTree(
@@ -93,7 +95,26 @@ func Test_Parse(t *testing.T) {
 001070         10  DUMMY-GROUP-2-OBJECT-D       PIC X.                  00000219
 001130         10  DUMMY-GROUP-2-OBJECT-E       PIC XXXX.               00000225
 001140         10  DUMMY-GROUP-2-OBJECT-F       REDEFINES               00000226
-001150              DUMMY-GROUP-2-OBJECT-E      PIC XXXX.               00000227`)),
+001150              DUMMY-GROUP-2-OBJECT-E      PIC XXXX.               00000227
+		`)),
+		},
+		{
+			name: "SimpleOccurs",
+			in: NewTree(
+				New("test",
+					`000160     05  DUMMY-GROUP-1.                                           00000115
+000170         10  DUMMY-SUB-GROUP-1.                                   00000116
+000180             15  DUMMY-GROUP-1-OBJECT-A   PIC 9  OCCURS 12.       00000117
+`)),
+		}, {
+			name: "MultilineOccurs",
+			in: NewTree(
+				New("test",
+					`000160     05  DUMMY-GROUP-1.                             00000115
+000170         10  DUMMY-SUB-GROUP-1.                        00000116
+000180             15  DUMMY-GROUP-1-OBJECT-A   PIC 9        00000117
+001300             OCCURS 12.                                00000242
+`)),
 		},
 	}
 
