1cce8c658dfe61c1de810cf1154d873c7138b2b7
mitchep1 <patrick.mitchell@anz.com>
2021-01-26 17:40:30 +1100

    Update example files

--- example/example.go
+++ example/example.go
@@ -7,20 +7,36 @@
 package main
 
 // Copybookexample contains a representation of your provided Copybook
-type Copybookexample struct {
-	DUMMYGROUP1OBJECTA    uint     `pic:"4"`     // start:1 end:4
-	DUMMYGROUP1OBJECTB    string   `pic:"1"`     // start:5 end:5
-	DUMMYGROUP1OBJECTC    uint     `pic:"4"`     // start:6 end:9
-	DUMMYGROUP1OBJECTD    string   `pic:"40"`    // start:10 end:49
-	DUMMYGROUP1OBJECTE    string   `pic:"8"`     // start:50 end:57
-	DUMMYGROUP1OBJECTG    string   `pic:"2"`     // start:58 end:59
-	DUMMYGROUP1OBJECTH    uint     `pic:"4"`     // start:60 end:63
-	DUMMYGROUP2OBJECTA    float64  `pic:"14"`    // start:64 end:77
-	DUMMYGROUP2OBJECTB    uint     `pic:"7"`     // start:78 end:84
-	DUMMYGROUP2OBJECTC    string   `pic:"4"`     // start:85 end:88
-	DUMMYGROUP2OBJECTD    string   `pic:"1"`     // start:89 end:89
-	DUMMYGROUP2OBJECTF    string   `pic:"7"`     // start:90 end:96
+type root struct {
+	DUMMYGROUP1 DUMMYGROUP1
+	DUMMYGROUP3 DUMMYGROUP3
+}
+
+type DUMMYSUBGROUP1 struct {
+	DUMMYGROUP1OBJECTA uint   `pic:"4"`  // start:1 end:4
+	DUMMYGROUP1OBJECTB string `pic:"1"`  // start:5 end:5
+	DUMMYGROUP1OBJECTC uint   `pic:"4"`  // start:6 end:9
+	DUMMYGROUP1OBJECTD string `pic:"40"` // start:10 end:49
+	DUMMYGROUP1OBJECTE string `pic:"8"`  // start:50 end:57
+	DUMMYGROUP1OBJECTG string `pic:"2"`  // start:58 end:59
+	DUMMYGROUP1OBJECTH uint   `pic:"4"`  // start:60 end:63
+}
+
+type DUMMYGROUP1 struct {
+	DUMMYSUBGROUP1 DUMMYSUBGROUP1
+}
+
+type DUMMYSUBGROUP2GETSDROPPED struct {
 	DUMMYSUBGROUP2OBJECTA []string `pic:"12,12"` // start:97 end:240
-	DUMMYGROUP2OBJECTG    string   `pic:"12"`    // start:241 end:252
-	DUMMYGROUP2OBJECTH    string   `pic:"12"`    // start:253 end:264
+}
+
+type DUMMYGROUP3 struct {
+	DUMMYGROUP2OBJECTA        string `pic:"14"` // start:64 end:77
+	DUMMYGROUP2OBJECTB        uint   `pic:"7"`  // start:78 end:84
+	DUMMYGROUP2OBJECTC        string `pic:"4"`  // start:85 end:88
+	DUMMYGROUP2OBJECTD        string `pic:"1"`  // start:89 end:89
+	DUMMYGROUP2OBJECTF        string `pic:"7"`  // start:90 end:96
+	DUMMYSUBGROUP2GETSDROPPED DUMMYSUBGROUP2GETSDROPPED
+	DUMMYGROUP2OBJECTG        string `pic:"12"` // start:241 end:252
+	DUMMYGROUP2OBJECTH        string `pic:"12"` // start:253 end:264
 }
